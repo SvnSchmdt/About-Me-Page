dba236013fdffc86b58d7680a56be1b8f0c2aee1
SvnSchmdt <sven.schmidt1502@gmail.com>
2023-09-28 11:43:46 +0200

    added font color changing when switchbox true

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,7 +1,19 @@
 function toggleDarkMode(){
-    var element = document.getElementsByClassName("main");
-    console.log(element[0].classList);
-    element[0].classList.toggle("dark_mode");
+    // Change background color
+    var mainElement = document.getElementsByClassName("main");
+    mainElement[0].classList.toggle("dark_mode");
+
+    // change header font color
+    var headerElement = document.getElementsByClassName("header");
+    headerElement[0].classList.toggle("header_dark");
+
+    // change body font color
+    var bodyElement = document.getElementsByClassName("content");
+    bodyElement[0].classList.toggle("content_dark");
+
+    // change footer font color
+    var footerElement = document.getElementsByClassName("copyright");
+    footerElement[0].classList.toggle("copyright_dark");
 
 }
 
